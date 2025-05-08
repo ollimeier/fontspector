b782afe9fb37915a3bd0ffadb911be8083236f42
Simon Cozens <simon@simon-cozens.org>
2025-05-08 11:28:08 +0100

    "But it works on my computer". Damned case-insensitive filesystems.

--- fontspector-py/tests/test_checks_googlefonts.py
+++ fontspector-py/tests/test_checks_googlefonts.py
@@ -2477,10 +2477,10 @@ def test_check_repo_dirname_matches_nameid_1(check, tmp_path):
     src_family = portable_path(f"data/test/{FONT_FAMILY_NAME}")
     assert os.path.exists(os.path.join(src_family, "Rosarivo-Regular.ttf"))
     shutil.copytree(src_family, tmp_gf_dir, dirs_exist_ok=True)
-    assert os.path.exists(tmp_gf_dir / f"{FONT_FAMILY_NAME}-Regular.ttf")
+    assert os.path.exists(tmp_gf_dir / f"Rosarivo-Regular.ttf")
 
     # PASS result; only check regular as we get skips for non-Regular
-    fonts = [str(tmp_gf_dir / f"{FONT_FAMILY_NAME}-Regular.ttf")]
+    fonts = [str(tmp_gf_dir / f"Rosarivo-Regular.ttf")]
     assert_PASS(check(fonts))
 
     # Get the path of the Regular font; it will be used for deleting the file later.
@@ -2492,7 +2492,7 @@ def test_check_repo_dirname_matches_nameid_1(check, tmp_path):
     os.replace(tmp_gf_dir, renamed_tmp_gf_dir)
 
     # FAIL ("mismatch") result
-    fonts = [str(renamed_tmp_gf_dir / f"{FONT_FAMILY_NAME}-Regular.ttf")]
+    fonts = [str(renamed_tmp_gf_dir / f"Rosarivo-Regular.ttf")]
     msg = assert_results_contain(check(fonts), FAIL, "mismatch")
     assert msg == (
         f"Family name on the name table ('{FONT_FAMILY_NAME.title()}')"
