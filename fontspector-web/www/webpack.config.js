fd6fe619381e3455fb0fb86dc35a59c502e68385
Simon Cozens <simon@simon-cozens.org>
2025-05-14 15:43:30 +0100

    Custom profiles in WASM (#109)
    * Finish adding overrides
    
    * Make these WASMable
    
    * Add everything apart from typenetwork
    
    * Mild rebranding
    
    * Update Cargo.lock
    
    * Don't build fs-web when testing

--- fontspector-web/www/webpack.config.js
+++ fontspector-web/www/webpack.config.js
@@ -1,9 +1,10 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 const crypto = require("crypto");
 const { experiments } = require("webpack");
 const crypto_orig_createHash = crypto.createHash;
-crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+crypto.createHash = (algorithm) =>
+  crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
 
 module.exports = {
   entry: "./bootstrap.js",
@@ -11,34 +12,33 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
     fallback: {
-      "path": false,
-      "fs": false,
-    }
+      path: false,
+      fs: false,
+    },
   },
   output: {
     path: path.resolve(__dirname, "..", "..", "docs"),
-    filename: "bootstrap.js"
+    filename: "bootstrap.js",
   },
   experiments: {
-    asyncWebAssembly: true
+    asyncWebAssembly: true,
   },
   mode: "development",
   plugins: [
     new CopyWebpackPlugin([
-        'index.html',
-        '*.css',
-        '*.png',
-        'cmark-gfm.js',
-        'node_modules/harfbuzzjs/hb.wasm',
-       ]
-    )
-  ]
+      "index.html",
+      "*.css",
+      "*.svg",
+      "cmark-gfm.js",
+      "node_modules/harfbuzzjs/hb.wasm",
+    ]),
+  ],
 };
