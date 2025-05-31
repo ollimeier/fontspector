c1e39a79075ea51b6265ac7238f04a8552780452
Olli Meier <hallo@olli-meier.de>
2025-05-31 18:06:25 +0200

    Update fontwerk profile similar to fontbakery

--- profile-fontwerk/src/lib.rs
+++ profile-fontwerk/src/lib.rs
@@ -1,16 +1,32 @@
 #![deny(clippy::unwrap_used, clippy::expect_used)]
 mod checks;
 
-use fontspector_checkapi::{Override, ProfileBuilder, Registry, StatusCode};
+use fontspector_checkapi::{ProfileBuilder, Registry};
 
 pub struct Fontwerk;
 impl fontspector_checkapi::Plugin for Fontwerk {
     fn register(&self, cr: &mut Registry) -> Result<(), String> {
         let builder = ProfileBuilder::new()
-            .add_section("Fontwerk Fonts Checks")
-            // "Fontwerk Fonts Checks" = ["fontwerk/vendor_id"]
-            .add_and_register_check(checks::fontwerk::vendor_id)
-            .include_profile("universal");
+            .include_profile("googlefonts")
+            // exclude googlefonts checks
+            .exclude_check("googlefonts/canonical_filename")
+            .exclude_check("googlefonts/family/italics_have_roman_counterparts")  // May need some improvements before we decide to include this one.
+            .exclude_check("googlefonts/font_copyright")
+            .exclude_check("googlefonts/fstype")
+            .exclude_check("googlefonts/gasp")
+            .exclude_check("googlefonts/metadata/includes_production_subsets")
+            .exclude_check("googlefonts/meta/script_lang_tags")
+            .exclude_check("googlefonts/name/description_max_length")
+            .exclude_check("googlefonts/name/line_breaks")
+            .exclude_check("googlefonts/production_glyphs_similarity")
+            .exclude_check("googlefonts/vendor_id") // Custom fontwerk test below
+            .exclude_check("googlefonts/version_bump")
+            .exclude_check("fontdata_namecheck")
+            .add_section("Fontwerk Checks")
+            .add_and_register_check(checks::fontwerk::vendor_id);
+            // TODO: implement other Fontwerk checks
+            // .add_and_register_check("fontwerk/names_match_default_fvar")
+            // .add_and_register_check("fontwerk/style_linking");
         builder.build("fontwerk", cr)
     }
 }
