a1ef341a1a5f4a106de3b2ce3a213b8dc30d0de9
Olli Meier <hallo@olli-meier.de>
2025-06-19 12:29:33 +0200

    Fix bold_italic_unique_for_nameid1

--- profile-opentype/src/checks/opentype/family/bold_italic_unique_for_nameid1.rs
+++ profile-opentype/src/checks/opentype/family/bold_italic_unique_for_nameid1.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 
 use fontations::skrifa::raw::tables::os2::SelectionFlags;
+use fontations::skrifa::string::StringId;
 use fontspector_checkapi::{prelude::*, FileTypeConvert};
 
 #[check(
@@ -19,23 +20,26 @@ use fontspector_checkapi::{prelude::*, FileTypeConvert};
 fn bold_italic_unique_for_nameid1(c: &TestableCollection, _context: &Context) -> CheckFnResult {
     let fonts = TTF.from_collection(c);
     let mut problems = vec![];
-    let mut flags: HashSet<(bool, bool)> = HashSet::new();
+    let mut flags: HashSet<(bool, bool, String)> = HashSet::new();
     let ribbi = fonts.iter().filter(|f| f.is_ribbi());
     for font in ribbi {
         let fsselection = font.get_os2_fsselection()?;
+        let name_id_1: Vec<_> = font.get_name_entry_strings(StringId::FAMILY_NAME).collect();
         let val = (
             fsselection.intersects(SelectionFlags::BOLD),
             fsselection.intersects(SelectionFlags::ITALIC),
+            name_id_1[0].clone(), // use the first name id 1 entry
         );
         if flags.contains(&val) {
             problems.push(Status::fail(
                 "unique-fsselection",
                 &(format!(
-                    "Font {} has the same selection flags ({}{}{}) as another font",
+                    "Font {} has the same selection flags ({}{}{}) as another font ({})",
                     font.filename.to_string_lossy(),
                     if val.0 { "bold" } else { "" },
                     if val.0 && val.1 { " & " } else { "" },
-                    if val.1 { "italic" } else { "" }
+                    if val.1 { "italic" } else { "" },
+                    val.2,
                 )),
             ));
         } else {
